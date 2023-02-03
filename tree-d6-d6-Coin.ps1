a1cabec7b1dbb5cfd1fd2d0036b393a50ea40531
M-Nelyubov <54564357+M-Nelyubov@users.noreply.github.com>
2023-02-02 22:25:53 -0500

    Create tree-d6-d6-Coin.ps1

---
+++ tree-d6-d6-Coin.ps1
@@ -0,0 +1,51 @@
+﻿Import-Module PSGraph
+
+
+class Connection {
+    [string] $start
+    [string] $end
+    [string] $label
+    
+    Connection ($start, $end, $label="") {
+        $this.start = $start
+        $this.end   = $end
+        $this.label = $label
+    }
+}
+
+$edges = @()
+
+
+[string[]]$latestWave = @("Start")
+
+
+$latestWave = $latestWave | foreach {
+    $rootNode = $_
+    1..6 | foreach {
+        $edges += [Connection]::new($rootNode, $_, $_)
+        "$_"
+    }
+}
+
+$latestWave = $latestWave | foreach {
+    $rootNode = $_
+    1..6 | foreach {
+        $edges += [Connection]::new($rootNode, "$rootNode, $_", $_)
+        "$rootNode, $_"
+    }
+}
+
+$latestWave = $latestWave | foreach {
+    $rootNode = $_
+    @("H","T") | foreach {
+        $edges += [Connection]::new($rootNode, "$rootNode, $_", $_)
+        "$rootNode, $_"
+    }
+}
+
+
+graph d -Attributes @{dpi=200; fontsize=20; compound=$true} {
+    $edges | foreach {
+        edge $_.start $_.end -Attributes @{label=$_.label}
+    }
+} | Export-PSGraph -ShowGraph -LayoutEngine Hierarchical -DestinationPath "$PSScriptRoot\images\render-$((Get-Date).toString('yyyy-MM-dd_hh-mm_ss')).png"
